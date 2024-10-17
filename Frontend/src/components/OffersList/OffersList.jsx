63954cce3c4c75605a400e7505b9f0e19f12fb65
MARIO HUARACHA <1822894@cmontmorency.qc.ca>
2024-10-17 16:40:19 -0400

    Erreur: L'affichage des offres de l'employeur n'apparaissent pas

--- Frontend/src/components/OffersList/OffersList.jsx
+++ Frontend/src/components/OffersList/OffersList.jsx
@@ -6,16 +6,17 @@ import { useHttpClient } from "../../hooks/http-hook";
 import { NavLink, useLocation } from "react-router-dom";
 import axios from "axios";
 
-
 const OffersList = () => {
-
   const [offres, setOffres] = useState([]);
   const [type, setType] = useState("");
   const [error, setError] = useState(null);
   const auth = useContext(AuthContext);
-  const { sendRequest } = useHttpClient();
+  const id = auth.employeurId;
 
+  const { sendRequest } = useHttpClient();
   const location = useLocation();
+
+  const filtreEmp = offres.filter((offre) => offre.employeurId === id);
 
   // Trouve le type (Employeur ou Candidat)
   useEffect(() => {
@@ -42,7 +43,9 @@ const OffersList = () => {
   useEffect(() => {
     const fetchOffres = async () => {
       try {
-        const response = await axios.get(process.env.REACT_APP_BACKEND_URL +"offres/");
+        const response = await axios.get(
+          process.env.REACT_APP_BACKEND_URL + "offres/"
+        );
         setOffres(response.data.offres);
       } catch (err) {
         setError("Erreur lors de la récupération des offres.");
@@ -55,41 +58,51 @@ const OffersList = () => {
   if (offres.length === 0) {
     return (
       <div>
-        {type == "Employeur" ? (
+        {type === "Employeur" ? (
           <NavLink to="/add-offer">Ajouter une offre</NavLink>
         ) : (
-          <div></div>
+          <div>
+            <h2>Aucune offre disponible</h2>
+          </div>
         )}
-        <h2>Aucune offre disponible</h2>
       </div>
     );
   }
 
-
   return (
     <div>
-      {type == "Employeur" ? (
-        <NavLink to="/add-offer">Ajouter une offre</NavLink>
+      {type === "Employeur" ? (
+        <div>
+          <NavLink to="/add-offer">Ajouter une offre</NavLink>
+          {filtreEmp.map((offer) => (
+            <OffersItem
+              key={offer.id}
+              titre={offer.titre}
+              nomEmployeur={offer.nomEmployeur}
+              email={offer.email}
+              salaire={offer.salaire}
+              details={offer.details}
+              employeurId={offer.employeurId}
+            />
+          ))}
+        </div>
       ) : (
-        <div></div>
+        <div>
+          <ul>
+            {offres.map((offer) => (
+              <OffersItem
+                key={offer.id}
+                titre={offer.titre}
+                nomEmployeur={offer.nomEmployeur}
+                email={offer.email}
+                salaire={offer.salaire}
+                details={offer.details}
+                employeurId={offer.employeurId}
+              />
+            ))}
+          </ul>
+        </div>
       )}
-      <ul>
-        {offres.map((offer) => (
-          <OffersItem
-            key={offer.id}
-            titre={offer.titre}
-            nomEmployeur={offer.nomEmployeur}
-            email={offer.email}
-            salaire={offer.salaire}
-            details={offer.details}
-            employeurId={offer.employeurId}
-
-          />
-        ))}
-      </ul>
-
-      
-
     </div>
   );
 };
